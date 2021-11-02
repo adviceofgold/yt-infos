0ecb83da6f398b8d9bba0073e64f05b00bdf5f8c
adviceofgold <93557721+adviceofgold@users.noreply.github.com>
2021-11-02 21:38:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.get('/', async function(req, res) {
             //playlistId = $('body > script').text();
 	    //playlistId = $('body > script:nth-child(16)').html();
 	    //playlistId = JSON.stringify(JSON.parse(playlist),null,2);  
-      	    playlistId = jQuery.parseJSON($('body > script:nth-child(16)').html().substring(21).replace(';','').toString());
+      	    playlistId = jQuery.parseJSON($('body > script:nth-child(16)').html().substring(21).replace(';',''));
 	    //tmp = jQuery.parseJSON(jsonData);
       	    //jsonData = $('body > script:nth-child(16)').html().substring(21).replace(';','');
 	    //playlistId = jsonData.contents.twoColumnBrowseResultsRenderer.tabs[0].tabRenderer.content.sectionListRenderer.contants[0].itemSectionRenderer.contents[0].shelfRender.playAllButton.buttonRender.navigationEndpoint.watchEndpoint.playlistId;

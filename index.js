b0ad1834e5569856112dd9c6070d109796886d60
adviceofgold <93557721+adviceofgold@users.noreply.github.com>
2021-11-02 14:30:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,8 @@ app.get('/', async function(req, res) {
           if(!error) {
             $ = cheerio.load(html);
             channelId = $('meta[itemprop="channelId"]').attr('content');
-            playlistId = $('body > script')[14].html();
+	    playlistId = ($.match(/ytInitialData[^{]*(.*?);\s*<\/script>/s))[1];
+            //playlistId = $('body > script')[14].html();
 	   // playlistId = JSON.parse($('script[32]').textContent);
             //playlistId = $('script[32]').split('{"url":"/playlist?list=')[1].split('\u0026playnext', 1)[0].split('"', 1)[0];
 	    //playlistId = $('div[id="play-button"] > ytd-button-renderer > a').attr('href');

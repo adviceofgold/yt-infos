55cbe5e578e63f2a0e9a8b6222d99c035b05ac0c
adviceofgold <93557721+adviceofgold@users.noreply.github.com>
2021-11-04 03:09:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -25,20 +25,6 @@ app.get('/', async function(req, res) {
             $ = cheerio.load(html);
             channelId = $('meta[itemprop="channelId"]').attr('content');
 		  
-	    //playlistId = $('body > script:nth-child(16)').html().match(/ytInitialData[^{]*(.*?);\s*<\/script>/s)[1];
-      	    //playlistId = $('body > script:nth-child(16)').html().substring(21).replace(';','');
-      	    //tmp = $('body > script:nth-child(16)').html().toString();
-	    //const matchX = tmp.match(/var ytInitialData = (.*);/);
-	    //const jsonData = JSON.parse(matchX[1]);
-      	    //jsonData = $('body > script:nth-child(16)').html().substring(21).replace(';','');
-	    //playlistId = jsonData.contents.twoColumnBrowseResultsRenderer.tabs[0].tabRenderer.content.sectionListRenderer.contants[0].itemSectionRenderer.contents[0].shelfRender.playAllButton.buttonRender.navigationEndpoint.watchEndpoint.playlistId;
-	    //playlistId = jsonData["contents"]["twoColumnBrowseResultsRenderer"]["tabs"][0]["tabRenderer"]["content"]["sectionListRenderer"]["contants"][0]["itemSectionRenderer"]["contents"][0]["shelfRender"]["playAllButton"]["buttonRender"]["navigationEndpoint"]["watchEndpoint"]["playlistId"];
-	    //playlistId = matchX[1];
-	    //playlistId = $('script[32]').split('{"url":"/playlist?list=')[1].split('\u0026playnext', 1)[0].split('"', 1)[0];
-	    //playlistId = $('div[id="play-button"] > ytd-button-renderer > a').attr('href');
-	    // playlistId = $('#play-button > ytd-button-renderer > a').attr('href');
-            //Subscribers = $('#subscriber-count').attr('aria-label').split(" ")[0];
-            //Subscribers = $('#subscriber-count').text();
             resolve({
                 channelId: channelId,
                 //playlistId: playlistId,
